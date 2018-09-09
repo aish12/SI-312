1b556e7953fbc7c21cfa3309d9fcfc4a3909c9b8
aish12 <ashashidhar@utexas.edu>
2018-09-09 10:44:21 -0500

    Create BadStyle.java

---
+++ Week2/BadStyle.java
@@ -0,0 +1,23 @@
+public class hello {
+
+	int a=5;
+	public static void main(String[] args) {
+
+	}
+
+	public static void Add(int x, int y) {
+		System.out.println(x+y);
+	}
+
+public static void lksdfjlhgskdjfg(int x, int y) {
+System.out.println(x-y);
+}
+	public static void printNumber {
+		System.out.println(a);
+	}
+
+	public static void printCircleArea(int diameter) {
+		System.out.println(3.14*diameter);
+	}
+
+}
